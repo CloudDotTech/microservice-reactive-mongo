98eb2d491f6257601a4eb85a3cfced200a66e963
First Last <email.mail.com>
2021-06-04 22:34:40 -0500

    Commit

---
+++ src/main/java/com/cloud/microservicereactivemongo/MicroserviceReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.microservicereactivemongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceReactiveMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/microservicereactivemongo/MicroserviceReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.microservicereactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@RunWith(SpringRunner.class)
+@WebFluxTest(ProductController.class)
+class MicroserviceReactiveMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
